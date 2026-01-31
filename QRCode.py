7b27ebb2881d55ffcff4cbfc8fefd94420a89f73
SudhanshuDash <109430506+SudhanshuDash@users.noreply.github.com>
2026-01-31 15:35:43 -0600

    Add files via upload

---
+++ QRCode.py
@@ -0,0 +1,18 @@
+import qrcode
+
+url = "https://www.bioxsystems.com/"
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4
+)
+
+qr.add_data(url)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("bioxsystems_qrcode.png")
+img.show()
+
